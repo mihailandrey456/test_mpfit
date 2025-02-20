c4f1a2a1d27df673aa288e94c860bd86f26c3161
Andrew Mikhailov <cmikj@yandex.ru>
2025-02-20 17:53:45 +0300

    categories

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $categories = ['Легкий', 'Хрупкий', 'Тяжелый'];
+        foreach ($categories as $categoryName) {
+            $category = new Category();
+            $category->name = $categoryName;
+            $category->saveOrFail();
+        }
     }
 }
